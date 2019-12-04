24c62ac99865e311d9f392a050063a4ac066cd10
admin <nikolakaraklic@gmail.com>
2019-12-04 13:56:16 +0100

    Added initial app structure.

---
+++ BankApp/BankService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
