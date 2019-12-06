3d37416b107901b713d6e5b8d73d44e67c9a0926
admin <jovanov33@gmail.com>
2019-12-06 17:42:01 +0100

    [BankService] Certificate client proxy added.

---
+++ BankApp/Common/Communication/CertificateClientProxy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Common.Communication
+{
+	/// <summary>
+	/// Represents client proxy for given interface with certificate management.
+	/// </summary>
+	/// <typeparam name="T">Interface which will be used for proxy.</typeparam>
+	public class CertificateClientProxy<T> : ChannelFactory<T>, IDisposable 
+		where T : new()
+	{
+		/// <summary>
+		/// Initializes new instance of <see cref="CertificateClientProxy<T>> class."/> 
+		/// </summary>
+		/// <param name="binding">Service binding.</param>
+		/// <param name="address">Service endpoint.</param>
+		public CertificateClientProxy(NetTcpBinding binding, EndpointAddress address) : base(binding, address)
+		{
+			// todo add certificate
+
+			Proxy = this.CreateChannel();
+		}
+
+		/// <summary>
+		/// Proxy used for communication with service.
+		/// </summary>
+		public T Proxy { get; private set; }
+
+		/// <inheritdoc/>
+		public void Dispose()
+		{
+			if (Proxy != null)
+			{
+				Proxy = default(T);
+			}
+
+			this.Close();
+		}
+	}
+}
