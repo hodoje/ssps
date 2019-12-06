cdf87d1b0da9faac1eb380d826f47591d97731c4
admin <jovanov33@gmail.com>
2019-12-06 17:43:12 +0100

    [BankService] Added bank service configuration. BankService class added.

---
+++ BankApp/BankService/BankingService.cs
@@ -0,0 +1,70 @@
+﻿using Common.Commanding;
+using Common.ServiceInterfaces;
+
+namespace BankService
+{
+	/// <summary>
+	/// Class represents banking service.
+	/// </summary>
+	public class BankingService : IUserService, IAdminService
+	{
+		private CommandingManager.CommandingManager commandManager;
+		private object locker = new object();
+		private long commandIdNumber = 0;
+
+		public BankingService()
+		{
+			commandManager = new CommandingManager.CommandingManager();
+		}
+
+		public void Deposit(double amount)
+		{
+			// authorization and authentication
+			string username = null; // get username
+			long newId = GetUniqueId();
+
+			DepositCommand depositCommand = new DepositCommand(newId, username, amount);
+			commandManager.EnqueueCommand(depositCommand);
+		}
+
+		public void Register(string username, string password)
+		{
+			// authorization and authentication
+			long newId = GetUniqueId();
+
+			RegistrationCommand registrationCommand = new RegistrationCommand(newId, username, password);
+			commandManager.EnqueueCommand(registrationCommand);
+		}
+
+		public void RequestLoan(double amount)
+		{
+			// authorization and authentication
+			string username = null; // get username
+			long newId = GetUniqueId();
+
+			RequestLoanCommand requestLoanCommand = new RequestLoanCommand(newId, username, amount);
+			commandManager.EnqueueCommand(requestLoanCommand);
+		}
+
+		public void Withdraw(double amount)
+		{
+			// authorization and authentication
+			string username = null; // get username
+			long newId = GetUniqueId();
+
+			WithdrawCommand withdrawCommand = new WithdrawCommand(newId, username, amount);
+			commandManager.EnqueueCommand(withdrawCommand);
+		}
+
+		private long GetUniqueId()
+		{
+			long newId;
+			lock (locker)
+			{
+				newId = ++commandIdNumber;
+			}
+
+			return newId;
+		}
+	}
+}
---
+++ BankApp/BankService/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace BankService
+{
+	public struct ConnectionInfo
+	{
+		EndpointAddress EndpointAddess { get; set; }
+		NetTcpBinding NetTcpBinding { get; set; }
+	}
+	internal class Configuration
+	{
+		private static Configuration instance;
+
+		static Configuration()
+		{
+			instance = new Configuration();
+			// todo Connections
+		}
+
+		public static Configuration Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		public Dictionary<Type, ConnectionInfo> Connections { get; set; }
+	}
+}
