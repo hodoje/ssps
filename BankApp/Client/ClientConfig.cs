640a6337138efa71bc7a5f90b360b3754ac2c016
admin <nikolakaraklic@gmail.com>
2019-12-10 12:05:05 +0100

    Updated all configs. Swapped Configuration.cs for BankServiceConfig.cs

--- BankApp/Client/ClientConfig.cs
+++ BankApp/Client/ClientConfig.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
-	class ClientConfig
+	/// <summary>
+	/// Loads app settings from App.config
+	/// </summary>
+	public static class ClientConfig
 	{
+		public const string BankServiceAddressConfigName = "BankServiceAddress";
+		public const string BankServiceEndpointNameConfigName = "BankServiceEndpointName";
+
+		static ClientConfig()
+		{
+			BankServiceAddress = ConfigurationManager.AppSettings[BankServiceAddressConfigName];
+			BankServiceEndpointName = ConfigurationManager.AppSettings[BankServiceEndpointNameConfigName];
+		}
+
+		public static string BankServiceAddress { get; }
+		public static string BankServiceEndpointName { get; }
 	}
 }
