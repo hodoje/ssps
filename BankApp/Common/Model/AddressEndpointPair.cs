b88a91c11d3ef40c3fd21391d83f3002b943ae1d
Aleksandar Jovanov <jovanov33@gmail.com>
2019-12-14 23:00:48 +0100

    [Common] Model changed for config.

--- BankApp/Common/Model/AddressEndpointPair.cs
+++ BankApp/Common/Model/AddressEndpointPair.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace Common.Model
 {
-	public class AddressEndpointPair
+	public class SectorAdditionalConfig
 	{
 		[JsonProperty("address")]
 		public string Address { get; set; }
 		[JsonProperty("endpointName")]
 		public string EndpointName { get; set; }
+		[JsonProperty("queueSize")]
+		public int QueueSize { get; set; }
 	}
 }
