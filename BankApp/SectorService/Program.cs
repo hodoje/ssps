8a8da31c0a4af7d90ad5804333f5edd58ef03532
Никола Караклић <nikolakaraklic@gmail.com>
2019-12-15 23:43:58 +0100

    Sector type is chosen when running sector process.

--- BankApp/SectorService/Program.cs
+++ BankApp/SectorService/Program.cs
@@ -1,6 +1,9 @@
-﻿using Common.Model;
+﻿using Common.Communication;
+using Common.Model;
+using Common.ServiceInterfaces;
 using SectorService.ServiceHosts;
 using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 
 namespace SectorService
@@ -9,24 +12,42 @@ namespace SectorService
 	{
 		static void Main(string[] args)
 		{
-			string sectorType = args[0];
-			Console.WriteLine(sectorType);
+			Dictionary<string, string> sectorTypes = new Dictionary<string, string>
+			{
+				["0"] = "accountSector",
+				["1"] = "loanSector",
+				["2"] = "transactionSector"
+			};
+			string sectorType = "";
+			while (!sectorTypes.ContainsKey(sectorType))
+			{
+				Console.WriteLine("Choose what sector you want to run:");
+				foreach(var sectorPair in sectorTypes)
+				{
+					Console.WriteLine($"[{sectorPair.Key}] - {sectorPair.Value.ToUpper()}");
+				}
+				sectorType = Console.ReadLine();
+			}
+
+			string chosenSectorType = sectorTypes[sectorType];
+
+			Console.WriteLine($"Starting {chosenSectorType.ToUpper()}...");
 
 			try
 			{
 				SectorAdditionalConfig sectorAdditionalConfig;
-				if (SectorConfig.SectorsConfigs.TryGetValue(sectorType, out sectorAdditionalConfig))
+				if (SectorConfig.SectorsConfigs.TryGetValue(chosenSectorType, out sectorAdditionalConfig))
 				{
 					var sectorHost = new SectorServiceServiceHost(sectorAdditionalConfig);
 					sectorHost.OpenService();
-
+					Console.WriteLine($"{chosenSectorType.ToUpper()} started.");
 					//WindowsClientProxy<IStartupConfirmationService> startupProxy = new WindowsClientProxy<IStartupConfirmationService>(
 					//	SectorConfig.StartupConfirmationServiceAddress, SectorConfig.StartupConfirmationServiceEndpointName);
-					//startupProxy.Proxy.ConfirmStartup(sectorType);
+					//startupProxy.Proxy.ConfirmStartup(chosenSectorType);
 				}
 				else
 				{
-					throw new Exception($"{sectorType} ServiceHost does not exist.");
+					throw new Exception($"{chosenSectorType.ToUpper()} ServiceHost does not exist.");
 				}
 			}
 			catch (Exception e)
