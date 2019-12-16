8883cd081ddb67739ca0d978432a6d92bf986c0d
fgbjoki <jovnaov33@gmail.com>
2019-12-16 13:36:49 +0100

    [BankService] Added command executor unit.

---
+++ BankApp/BankService/CommandExecutor/CommandExecutor.cs
@@ -0,0 +1,50 @@
+﻿using Common.Commanding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankService.CommandExecutor
+{
+    public class CommandExecutor : ICommandExecutor
+    {
+        private CancellationTokenSource cancellationToken = new CancellationTokenSource();
+        
+        // todo add DataManager<User> + DataManager<Account> from ServiceLocator
+
+        private CommandQueue commandingQueue;
+
+        public CommandExecutor(CommandQueue commandingQueue)
+        {
+            this.commandingQueue = commandingQueue;
+        }
+
+        public void Start()
+        {
+            Task listenerThread = new Task(WorkerThread);
+            listenerThread.Start();
+        }
+
+        public void Stop()
+        {
+            cancellationToken.Cancel();
+        }
+
+        private void WorkerThread()
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                BaseCommand command = commandingQueue.Dequeue();
+
+                if (command == null)
+                {
+                    continue;
+                }
+
+                // execute command
+            }
+        }
+    }
+}
